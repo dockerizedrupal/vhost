37b4282f6a7b1c67887adc7caeaa3a231f553875
Jürgen Viljaste <j.viljaste@gmail.com>
2014-12-18 01:33:40 +0200

    Update

--- src/build/modules/vhost/manifests/nginx/supervisor.pp
+++ src/build/modules/vhost/manifests/nginx/supervisor.pp
@@ -1,7 +1,7 @@
 class vhost::nginx::supervisor {
   file { '/etc/supervisor/conf.d/nginx.conf':
     ensure => present,
-    source => 'puppet:///modules/nginx/etc/supervisor/conf.d/nginx.conf',
+    source => 'puppet:///modules/vhost/etc/supervisor/conf.d/nginx.conf',
     mode => 644
   }
 }
