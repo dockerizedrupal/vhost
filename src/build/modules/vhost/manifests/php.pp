de2d1873d11fb7acf326f11b7f9a04932ed9b75c
Jürgen Viljaste <j.viljaste@gmail.com>
2014-12-28 23:36:56 +0200

    Update

--- src/build/modules/vhost/manifests/php.pp
+++ src/build/modules/vhost/manifests/php.pp
@@ -7,4 +7,10 @@ class vhost::php {
     source => 'puppet:///modules/vhost/etc/php5/fpm/php.ini',
     mode   => 644
   }
+
+  file { '/etc/php5/fpm/pool.d/www.conf':
+    ensure => present,
+    source => 'puppet:///modules/vhost/etc/php5/fpm/pool.d/www.conf',
+    mode   => 644
+  }
 }
