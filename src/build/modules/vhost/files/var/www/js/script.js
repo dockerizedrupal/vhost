b6c35f7adb7d85bf576f0f307df58d9b1bac349b
Jürgen Viljaste <j.viljaste@gmail.com>
2015-02-19 18:50:25 +0200

    Update

--- src/build/modules/vhost/files/var/www/js/script.js
+++ src/build/modules/vhost/files/var/www/js/script.js
@@ -1,5 +1,23 @@
 (function($) {
   $(function() {
+    $('#filter').val($.localStorage.get('filter'));
+
+    var filter = function() {
+      $.localStorage.set('filter', $('#filter').val());
+
+      $('.panel-group').each(function() {
+        $(this).show();
+
+        if ($(this).find('h2').text().toLowerCase().indexOf($('#filter').val().toLowerCase()) < 0) {
+          $(this).hide();
+        }
+      });
+    };
+
+    $('#filter').keyup(function() {
+      filter();
+    });
+
     $(document).on('show.bs.collapse', '.panel-collapse', function() {
       $.localStorage.set($(this).attr('id'), 1);
     });
@@ -17,11 +35,13 @@
     };
 
     setInterval(function() {
-      $('#page').load(location.href + ' #page > *', function() {
+      $('#content').load(location.href + ' #content > *', function() {
         keep_panels_open();
+        filter();
       });
     }, 1000);
 
     keep_panels_open();
+    filter();
   });
 })(jQuery);
